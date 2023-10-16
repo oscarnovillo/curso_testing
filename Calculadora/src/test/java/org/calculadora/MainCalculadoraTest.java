bc11e4faddcd835ebd65e422e26e05f7a270a7f6
juan <oscar.novillo@gmail.com>
2023-10-16 20:44:34 +0200

    Merge remote-tracking branch 'origin/CalculadoraASaco' into CalculadoraASaco

--- Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
+++ Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
@@ -2,18 +2,31 @@ package org.calculadora;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainCalculadoraTest {
 
     @Test
-    void main() {
+    void sumar() {
+        PrintStream stdout = System.out;
+        String entrada = "1\n1\n1\n5\n";
 
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
 
+        var byteArrayOutputStream  = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
 
         MainCalculadora.main(null);
 
+        String salida = byteArrayOutputStream.toString();
+
+        System.setOut(stdout);
 
 
+        assertEquals("El resultado es: 2",salida);
     }
 }
