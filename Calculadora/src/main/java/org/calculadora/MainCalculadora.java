7b21d507dd7208e8b3c590840263e1c9ba4a296c
juan <oscar.novillo@gmail.com>
2023-10-16 19:45:06 +0200

    Calculadora sin arquitectura

---
+++ Calculadora/src/main/java/org/calculadora/MainCalculadora.java
@@ -0,0 +1,73 @@
+package org.calculadora;
+
+import java.util.Scanner;
+
+public class MainCalculadora {
+
+    public static void main(String[] args) {
+
+
+        // Quiero una calculadora con un menu en un switch con sumar, restar, dividir y multiplicar
+        // Quiero que me pida dos numeros y me devuelva el resultado
+        // sacar un menu a consola con las opciones de una calculadora
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+            System.out.println("Bienvenido a la calculadora");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("5. Salir");
+            System.out.println("Introduce una opcion");
+            opcion = sc.nextInt();
+
+            switch (opcion)
+            {
+                case 1:
+                    System.out.println("Has elegido sumar");
+                    System.out.println("Introduce el primer numero");
+                    int num1 = sc.nextInt();
+                    System.out.println("Introduce el segundo numero");
+                    int num2 = sc.nextInt();
+                    System.out.println("El resultado es: " + (num1 + num2));
+                    break;
+                case 2:
+                    System.out.println("Has elegido restar");
+                    System.out.println("Introduce el primer numero");
+                    int num3 = sc.nextInt();
+                    System.out.println("Introduce el segundo numero");
+                    int num4 = sc.nextInt();
+                    System.out.println("El resultado es: " + (num3 - num4));
+                    break;
+                case 3:
+                    System.out.println("Has elegido multiplicar");
+                    System.out.println("Introduce el primer numero");
+                    int num5 = sc.nextInt();
+                    System.out.println("Introduce el segundo numero");
+                    int num6 = sc.nextInt();
+                    System.out.println("El resultado es: " + (num5 * num6));
+                    break;
+                case 4:
+                    System.out.println("Has elegido dividir");
+                    System.out.println("Introduce el primer numero");
+                    int num7 = sc.nextInt();
+                    System.out.println("Introduce el segundo numero");
+                    int num8 = sc.nextInt();
+                    System.out.println("El resultado es: " + (num7 / num8));
+                    break;
+                case 5:
+                    System.out.println("Has elegido salir");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+
+        } while (opcion != 5);
+
+
+    }
+
+}
---
+++ Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
@@ -0,0 +1,19 @@
+package org.calculadora;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainCalculadoraTest {
+
+    @Test
+    void main() {
+
+
+
+        MainCalculadora.main(null);
+
+
+
+    }
+}
