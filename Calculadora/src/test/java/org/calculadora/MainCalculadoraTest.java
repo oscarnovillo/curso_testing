d3e6eafa7c4a9cdf327e15cdb436fe264e532634
javier sebastian <javier.sebastian@educa.madrid.org>
2023-10-18 18:28:15 +0200

    javierS1 cambios

--- Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
+++ Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
@@ -16,6 +16,26 @@ class MainCalculadoraTest {
     @DisplayName("Suma de 1 y uno que da 2")
     void sumar() {
         PrintStream stdout = System.out;
+        String entrada = "2\n5\n3\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream  = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        MainCalculadora.main(null);
+
+        String salida = byteArrayOutputStream.toString();
+
+        System.setOut(stdout);
+
+
+        assertTrue(salida.contains("El resultado es: 2"));
+    }
+    @Test
+    @DisplayName("Resta de 5 y 3 que da 2")
+    void restar() {
+        PrintStream stdout = System.out;
         String entrada = "1\n1\n1\n5\n";
 
         System.setIn(new ByteArrayInputStream(entrada.getBytes()));
