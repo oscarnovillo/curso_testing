31e1d085b9303c60ddf7a64363fb657ed52094b6
Marcos <you@example.com>
2023-10-18 19:08:07 +0200

    test

--- Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
+++ Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
@@ -17,7 +17,7 @@ class MainCalculadoraTest {
 
         //Given
         PrintStream stdout = System.out;
-        String entrada = "1\n1\n5\n";
+        String entrada = "1\n1\n1\n5\n";
 
         System.setIn(new ByteArrayInputStream(entrada.getBytes()));
 
@@ -35,4 +35,78 @@ class MainCalculadoraTest {
         //comentario nuevo
         assertTrue(salida.contains("El resultado es: 2"));
     }
+    @Test
+    @DisplayName("Restar 1 y 1 para que de 1")
+    void restar_1_y_1_para_que_de_0() {
+
+        //Given
+        PrintStream stdout = System.out;
+        String entrada = "2\n1\n1\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream  = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        //When
+        MainCalculadora.main(null);
+
+        //Then
+        String salida = byteArrayOutputStream.toString();
+
+        System.setOut(stdout);
+
+        //comentario nuevo
+        assertTrue(salida.contains("El resultado es: 0"));
+    }
+    @Test
+    @DisplayName("Multiplicar 3 y 3 para que de 9")
+    void multiplicar_3_y_3_para_que_de_9() {
+
+        //Given
+        PrintStream stdout = System.out;
+        String entrada = "3\n3\n3\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream  = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        //When
+        MainCalculadora.main(null);
+
+        //Then
+        String salida = byteArrayOutputStream.toString();
+
+        System.setOut(stdout);
+
+        //comentario nuevo
+        assertTrue(salida.contains("El resultado es: 9"));
+    }
+    @Test
+    @DisplayName("Dividir 10 y 2 para que de 5")
+    void dividir_10_y_5_para_que_de_2() {
+
+        //Given
+        PrintStream stdout = System.out;
+        String entrada = "4\n10\n2\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream  = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        //When
+        MainCalculadora.main(null);
+
+        //Then
+        String salida = byteArrayOutputStream.toString();
+
+        System.setOut(stdout);
+
+        //comentario nuevo
+        assertTrue(salida.contains("El resultado es: 5"));
+    }
+
+
 }
