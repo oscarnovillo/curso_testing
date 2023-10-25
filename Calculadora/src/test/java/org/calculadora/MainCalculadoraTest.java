6c2b87c3e56c767df1fa9d7a21ca4086fdfd970f
Ramon Roca <rroca@ccoo.es>
2023-10-25 16:29:19 +0200

    Agregado test

--- Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
+++ Calculadora/src/test/java/org/calculadora/MainCalculadoraTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.InputMismatchException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,7 +22,7 @@ class MainCalculadoraTest {
 
         System.setIn(new ByteArrayInputStream(entrada.getBytes()));
 
-        var byteArrayOutputStream  = new ByteArrayOutputStream(1000);
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
         System.setOut(new PrintStream(byteArrayOutputStream));
 
         //When
@@ -37,7 +38,6 @@ class MainCalculadoraTest {
     }
 
 
-
     @Test
     @DisplayName("Restar 7 y 3 para que de 4")
     void restar_7_y_3_para_que_de_4() {
@@ -48,7 +48,7 @@ class MainCalculadoraTest {
 
         System.setIn(new ByteArrayInputStream(entrada.getBytes()));
 
-        var byteArrayOutputStream  = new ByteArrayOutputStream(1000);
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
         System.setOut(new PrintStream(byteArrayOutputStream));
 
         //When
@@ -63,5 +63,107 @@ class MainCalculadoraTest {
         assertTrue(salida.contains("El resultado es: 4"));
     }
 
+    // User selects option 1 (sum), inputs two numbers, and receives correct sum
+    @Test
+    public void test_sum_option() {
+        PrintStream stdout = System.out;
+        String entrada = "1\n2\n3\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        MainCalculadora.main(null);
+
+        String salida = byteArrayOutputStream.toString();
+
+        System.setOut(stdout);
+
+        assertTrue(salida.contains("El resultado es: 5"));
+    }
+
+    // User selects option 2 (subtract), inputs two numbers, and receives correct subtraction
+    @Test
+    public void test_subtract_option() {
+        PrintStream stdout = System.out;
+        String entrada = "2\n5\n3\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        MainCalculadora.main(null);
+
+        String salida = byteArrayOutputStream.toString();
+
+        System.setOut(stdout);
+
+        assertTrue(salida.contains("El resultado es: 2"));
+    }
+
+    // User selects option 3 (multiply), inputs two numbers, and receives correct multiplication
+    @Test
+    public void test_multiply_option() {
+        PrintStream stdout = System.out;
+        String entrada = "3\n2\n3\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        MainCalculadora.main(null);
+
+        String salida = byteArrayOutputStream.toString();
+
+        System.setOut(stdout);
+
+        assertTrue(salida.contains("El resultado es: 6"));
+    }
+
+    // User inputs non-integer value for menu option, program handles gracefully
+    @Test
+    public void test_non_integer_menu_option() {
+        PrintStream stdout = System.out;
+        String entrada = "a\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        assertThrows(InputMismatchException.class, () -> MainCalculadora.main(null));
+    }
+
+    // User inputs non-integer value for number input, program handles gracefully
+    @Test
+    public void test_non_integer_number_input() {
+        PrintStream stdout = System.out;
+        String entrada = "1\na\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+        assertThrows(InputMismatchException.class, () -> MainCalculadora.main(null));
+    }
+
+    // User selects option 4 (divide) and inputs 0 as second number, program handles gracefully
+    @Test
+    public void test_divide_option_with_zero() {
+        PrintStream stdout = System.out;
+        String entrada = "4\n5\n0\n5\n";
+
+        System.setIn(new ByteArrayInputStream(entrada.getBytes()));
+
+        var byteArrayOutputStream = new ByteArrayOutputStream(1000);
+        System.setOut(new PrintStream(byteArrayOutputStream));
+
+        assertThrows(ArithmeticException.class, () -> MainCalculadora.main(null));
+    }
 
 }
+
+
