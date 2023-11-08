4712020a09c8301b3920af1e1b8af58abcdb5042
oscarnovillo <oscar.novillo@gmail.com>
2023-11-08 18:49:27 +0100

    Auto stash before merge of "Gloria" and "develop"

---
+++ Calculadora/src/test/java/org/calculadora/SaludaTest.java
@@ -0,0 +1,12 @@
+package org.calculadora;
+
+import org.junit.jupiter.api.Test;
+
+public class SaludaTest {
+
+    @Test
+    void saluda() {
+        Saluda saluda = new Saluda();
+        saluda.saluda();
+    }
+}
